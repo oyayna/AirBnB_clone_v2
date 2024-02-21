8ef7776e6b0dfcc36a555a4a07efbc3c2bf53bce
amir-ee <melio@kali>
2024-02-21 18:52:52 +0100

    Update

--- models/state.py
+++ models/state.py
@@ -1,10 +1,10 @@
-# models/state.py
-from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String
-from sqlalchemy.orm import relationship
+#!/usr/bin/python3
+
+""" State Module for HBNB project """
+from models.base_model import BaseModel
 
 
-class State(BaseModel, Base):
-    __tablename__ = "states"
-    name = Column(String(128), nullable=False)
-    cities = relationship("City", backref="state", cascade="all, delete")
+class State(BaseModel):
+    """State class"""
+
+    name = ""
