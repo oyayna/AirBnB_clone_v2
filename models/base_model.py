32c4d0502da0579d618da7af7c6c1be9d5802645
amir-ee <melio@kali>
2024-02-21 19:30:26 +0100

    Update

--- models/base_model.py
+++ models/base_model.py
@@ -30,8 +30,8 @@ class BaseModel:
             from models import storage
 
             self.id = str(uuid.uuid4())
-            self.created_at = datetime.utcnow()
-            self.updated_at = datetime.utcnow()
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
             if kwargs:
                 for key, value in kwargs.items():
                     setattr(self, key, value)
@@ -46,7 +46,7 @@ class BaseModel:
 
         from models import storage
 
-        self.updated_at = datetime.utcnow()
+        self.updated_at = datetime.now()
         storage.save()
 
     def to_dict(self):
