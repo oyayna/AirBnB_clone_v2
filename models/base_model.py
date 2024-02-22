c89114cf8f5a0e5381ac90923d84f0036053e1be
amir-ee <melio@kali>
2024-02-22 19:55:37 +0100

    Update

--- models/base_model.py
+++ models/base_model.py
@@ -18,7 +18,7 @@ class BaseModel:
 
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
-            self.updated_at = datetime.now()
+            # self.updated_at = datetime.now()
             if kwargs:
                 for key, value in kwargs.items():
                     setattr(self, key, value)
@@ -33,12 +33,12 @@ class BaseModel:
 
         from models import storage
 
-        self.updated_at = datetime.now()
+        # self.updated_at = datetime.now()
         storage.save()
 
     def to_dict(self):
         obj_dict = self.__dict__.copy()
         obj_dict["__class__"] = self.__class__.__name__
         obj_dict["created_at"] = self.created_at.isoformat()
-        obj_dict["updated_at"] = self.updated_at.isoformat()
+        # obj_dict["updated_at"] = self.updated_at.isoformat()
         return obj_dict
