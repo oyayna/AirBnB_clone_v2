df231e0062cc38c29d306344e4bcfb111189e5c8
amir-ee <melio@kali>
2024-03-27 15:35:36 +0000

    Update

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -10,19 +10,19 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/', strict_slashes=False)
+@app.route("/", strict_slashes=False)
 def hbnb_route():
     """prints Hello HBNB"""
     return "Hello HBNB!"
 
 
-@app.route('/hbnb', strict_slashes=False)
+@app.route("/hbnb", strict_slashes=False)
 def hbnb():
     """prints HBNB"""
     return "HBNB"
 
 
-@app.route('/c/<string:text>', strict_slashes=False)
+@app.route("/c/<string:text>", strict_slashes=False)
 def c_text(text):
     """prints C followed by <text> content"""
     text = text.replace("_", " ")
@@ -30,4 +30,4 @@ def c_text(text):
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+    app.run(host="0.0.0.0", port=5000)
