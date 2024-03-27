183058ca70319e4b6b74762ba4ea5b75fc2ef028
amir-ee <melio@kali>
2024-03-27 16:03:38 +0000

    Update

--- models/state.py
+++ models/state.py
@@ -8,21 +8,20 @@ from sqlalchemy.orm import relationship
 from models import storage
 import os
 
-
 class State(BaseModel, Base):
-    """ State class / table model"""
-    __tablename__ = 'states'
-    if storage_type == 'db':
-        name = Column(String(128), nullable=False)
-        cities = relationship('City', backref='state',
-                              cascade='all, delete, delete-orphan')
-    else:
-        if os.getenv('HBNB_TYPE_STORAGE') != 'db':
-            @property
-            def cities(self):
-                """إرجاع قائمة المدن مرتبطة بالولاية"""
-                cities_list = []
-                for city in storage.all(City).values():
-                    if city.state_id == self.id:
-                        cities_list.append(city)
-                return cities_list
+    """State class"""
+
+    __tablename__ = "states"
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", cascade="all, delete", backref="state")
+
+    if os.getenv("HBNB_TYPE_STORAGE") != "db":
+
+        @property
+        def cities(self):
+            """إرجاع قائمة المدن مرتبطة بالولاية"""
+            cities_list = []
+            for city in storage.all(City).values():
+                if city.state_id == self.id:
+                    cities_list.append(city)
+            return cities_list
