99403bf2d52f64a198360fdaf0985cac98a430fd
amir-ee <melio@kali>
2024-03-28 01:28:07 +0000

    Update

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -12,38 +12,38 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route("/", strict_slashes=False)
+@app.route('/', strict_slashes=False)
 def hbnb_route():
     """prints Hello HBNB"""
     return "Hello HBNB!"
 
 
-@app.route("/hbnb", strict_slashes=False)
+@app.route('/hbnb', strict_slashes=False)
 def hbnb():
     """prints HBNB"""
     return "HBNB"
 
 
-@app.route("/c/<string:text>", strict_slashes=False)
+@app.route('/c/<string:text>', strict_slashes=False)
 def c_text(text):
     """prints C followed by <text> content"""
     text = text.replace("_", " ")
     return "C %s" % text
 
 
-@app.route("/python", strict_slashes=False)
-@app.route("/python/<string:text>", strict_slashes=False)
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<string:text>', strict_slashes=False)
 def python_text(text="is cool"):
     """prints Python is cool"""
     text = text.replace("_", " ")
     return "Python %s" % text
 
 
-@app.route("/number/<int:n>", strict_slashes=False)
+@app.route('/number/<int:n>', strict_slashes=False)
 def number_n(n):
     """display n if integer"""
     return "%i is a number" % n
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0")
